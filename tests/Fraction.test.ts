38cdda26a6938e79dc6085c5da6fe2fb978449d5
RWCannell <regancannell@gmail.com>
2023-06-25 12:21:13 +0200

    Simplified Fraction tests

--- tests/Fraction.test.ts
+++ tests/Fraction.test.ts
@@ -1,35 +1,29 @@
 import Fraction from "../lib/Fraction";
 
-let f: Fraction;
-let g: Fraction;
-let alpha: number;
-let beta: number;
+let frac: Fraction;
 
 describe("Tests for ThreeVector", () => {
     beforeEach(() => {
-        f = new Fraction(3, 4);
-        g = new Fraction(1, 6);
-        alpha = -5;
-        beta = 8;
+        frac = new Fraction(3, 4);
     });
 
     test("Should return correct value for 'add' function", () => {
         let result: Fraction = new Fraction(11, 12);
-        expect(f.add(1,6)).toStrictEqual(result);
+        expect(frac.add(1,6)).toStrictEqual(result);
     });
 
     test("Should return correct value for 'subtract' function", () => {
         let result: Fraction = new Fraction(7, 12);
-        expect(f.subtract(1,6)).toStrictEqual(result);
+        expect(frac.subtract(1,6)).toStrictEqual(result);
     });
 
     test("Should return correct value for 'multiply' function", () => {
         let result: Fraction = new Fraction(1, 8);
-        expect(f.multiply(1,6)).toStrictEqual(result);
+        expect(frac.multiply(1,6)).toStrictEqual(result);
     });
 
     test("Should return correct value for 'divide' function", () => {
         let result: Fraction = new Fraction(9, 2);
-        expect(f.divide(1,6)).toStrictEqual(result);
+        expect(frac.divide(1,6)).toStrictEqual(result);
     });
 });
